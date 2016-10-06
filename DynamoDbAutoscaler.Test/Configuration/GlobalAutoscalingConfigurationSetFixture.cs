4c62128be649aa22b373f71cc03312f011261b76
Lee Harold <lcharold@hotmail.com>
2016-10-06 10:51:35 -0500

    fix failing GlobalAutoscalingConfigurationSetFixture tests

--- DynamoDbAutoscaler.Test/Configuration/GlobalAutoscalingConfigurationSetFixture.cs
+++ DynamoDbAutoscaler.Test/Configuration/GlobalAutoscalingConfigurationSetFixture.cs
@@ -1,26 +1,40 @@
-﻿using DynamoDbAutoscaler.Configuration;
-using NUnit.Framework;
+﻿using System.IO;
 using System.Threading.Tasks;
+
+using DynamoDbAutoscaler.Configuration;
+using NUnit.Framework;
 
 namespace DynamoDbAutoscaler.Test.Configuration
 {
     [TestFixture]
     public class GlobalAutoscalingConfigurationSetFixture
-    {        
+    {
+        private static string GetLocalFilePath()
+        {
+            var basePath = TestContext.CurrentContext.TestDirectory;
+            var localFilePath = Path.Combine(basePath, "autoscaling.json");
+
+            return localFilePath;
+        }
 
         [Test]
         public async Task LoadFromFile_ShouldLoadAndSerializeConfiguration()
         {
-            var configuration = await GlobalAutoscalingConfigurationSet.LoadFromFileAsync(".\autoscaling.json");
+            var localFilePath = GetLocalFilePath();
+            var configuration = await GlobalAutoscalingConfigurationSet.LoadFromFileAsync(localFilePath);
 
             Assert.That(configuration.CheckInterval, Is.EqualTo(60));
             Assert.That(configuration.DemoMode, Is.EqualTo(true));
+            
+            // reads
             Assert.That(configuration.Reads.EnableAutoscaling, Is.EqualTo(true));
             Assert.That(configuration.Reads.UpperThreshold, Is.EqualTo(50));
             Assert.That(configuration.Reads.LowerThreshold, Is.EqualTo(25));
             Assert.That(configuration.Reads.ThrottleThreshold, Is.EqualTo(100));
             Assert.That(configuration.Reads.IncreaseWithPercent, Is.EqualTo(65));
             Assert.That(configuration.Reads.DecreaseWithPercent, Is.EqualTo(45));
+
+            // writes
             Assert.That(configuration.Writes.EnableAutoscaling, Is.EqualTo(true));
             Assert.That(configuration.Writes.UpperThreshold, Is.EqualTo(50));
             Assert.That(configuration.Writes.LowerThreshold, Is.EqualTo(25));
@@ -32,14 +46,19 @@ namespace DynamoDbAutoscaler.Test.Configuration
         [Test]
         public async Task LoadFromFile_ShouldLoadAndSerializeAllChildrenConfigurationAndSetGlobalWhenEmpty()
         {
-            var configuration = await GlobalAutoscalingConfigurationSet.LoadFromFileAsync();
+            var localFilePath = GetLocalFilePath();
+            var configuration = await GlobalAutoscalingConfigurationSet.LoadFromFileAsync(localFilePath);
 
             foreach (var child in configuration.TableConfigurations)
             {
                 Assert.That(child.DemoMode, Is.EqualTo(true));
+
+                // reads
                 Assert.That(child.Reads.EnableAutoscaling, Is.EqualTo(true));
                 Assert.That(child.Reads.MaxProvisioned, Is.GreaterThan(0));
                 Assert.That(child.Reads.MinProvisioned, Is.GreaterThan(0));
+
+                // writes
                 Assert.That(child.Writes.EnableAutoscaling, Is.EqualTo(true));
                 Assert.That(child.Writes.MaxProvisioned, Is.GreaterThan(0));
                 Assert.That(child.Writes.MinProvisioned, Is.GreaterThan(0));
@@ -48,9 +67,13 @@ namespace DynamoDbAutoscaler.Test.Configuration
             foreach (var child in configuration.GlobalSecondaryIndexConfigurations)
             {
                 Assert.That(child.DemoMode, Is.EqualTo(true));
+
+                // reads
                 Assert.That(child.Reads.EnableAutoscaling, Is.EqualTo(true));
                 Assert.That(child.Reads.MaxProvisioned, Is.GreaterThan(0));
                 Assert.That(child.Reads.MinProvisioned, Is.GreaterThan(0));
+
+                // writes
                 Assert.That(child.Writes.EnableAutoscaling, Is.EqualTo(true));
                 Assert.That(child.Writes.MaxProvisioned, Is.GreaterThan(0));
                 Assert.That(child.Writes.MinProvisioned, Is.GreaterThan(0));
